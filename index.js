3c7d2be69bd051ddf4283f38cd92105703736917
Gintaute Ezerskyte <gintaute.ez@gmail.com>
2017-02-19 14:52:09 +0200

    Add header css corrections

--- index.js
+++ index.js
@@ -1,48 +1,51 @@
 require ('./style/main.scss');
 
-// MENU TOGGLE FOR TABLET AND MOBILE
-document.getElementById('menu-icon').addEventListener('click', toggleMenu);
+document.addEventListener('DOMContentLoaded', function() {
+  // MENU TOGGLE FOR TABLET AND MOBILE
+  document.getElementById('menu-icon').addEventListener('click', toggleMenu);
 
-function toggleMenu() {
-  var currentClass = document.getElementById('nav-bar').classList;
-  if(currentClass.contains('hidden')) {
-    currentClass.remove('hidden');
-  } else {
-    currentClass.add('hidden');
+  function toggleMenu() {
+    var currentClass = document.getElementById('nav-bar').classList;
+    if(currentClass.contains('hidden-for-mobile')) {
+      currentClass.remove('hidden-for-mobile');
+    } else {
+      currentClass.add('hidden-for-mobile');
+    }
   }
-}
 
-// FORM VALIDATION
-var msgForm = document.getElementById('msg-form');
-msgForm.addEventListener('submit', validateForm);
+  // FORM VALIDATION
+  var msgForm = document.getElementById('msg-form');
+  msgForm.addEventListener('submit', validateForm);
 
-function validateForm(e) {
+  function validateForm(e) {
 
-  var name = document.getElementById('name').value;
-  var nameError = document.getElementById('errorName');
-  var lastName = document.getElementById('last-name').value;
-  var lastNameError = document.getElementById('errorLastName');
-  var userMessage = document.getElementById('msg').value;
-  var userMessageError = document.getElementById('errorMsg');
-  var errors = 0;
+    var name = document.getElementById('name').value;
+    var nameError = document.getElementById('errorName');
+    var lastName = document.getElementById('last-name').value;
+    var lastNameError = document.getElementById('errorLastName');
+    var userMessage = document.getElementById('msg').value;
+    var userMessageError = document.getElementById('errorMsg');
+    var errors = 0;
 
-  nameError.innerHTML = '';
-  lastNameError.innerHTML = '';
-  userMessageError.innerHTML = '';
+    nameError.innerHTML = '';
+    lastNameError.innerHTML = '';
+    userMessageError.innerHTML = '';
 
-  if(name == '') {
-    errors++;
-    nameError.innerHTML = 'Please specify your name.';
+    if(name == '') {
+      errors++;
+      nameError.innerHTML = 'Please specify your name.';
+    }
+    if(lastName == '') {
+      errors++;
+      lastNameError.innerHTML = 'Please specify your last name.';
+    }
+    if(userMessage == '') {
+      errors++;
+      userMessageError.innerHTML = 'Please enter your message.';
+    }
+    if(errors != 0) {
+      e.preventDefault();
+    }
   }
-  if(lastName == '') {
-    errors++;
-    lastNameError.innerHTML = 'Please specify your last name.';
-  }
-  if(userMessage == '') {
-    errors++;
-    userMessageError.innerHTML = 'Please enter your message.';
-  }
-  if(errors != 0) {
-    e.preventDefault();
-  }
-}
+});
+
